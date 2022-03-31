6ccbd07a538ad210a00bb7bc6d454dda4142ba69
Damian Molinski <tedeer@gmail.com>
2022-03-31 23:22:16 +0200

    Added method to upload file to server, but structuary

--- class/File.class.php
+++ class/File.class.php
@@ -1,17 +1,24 @@
 <?php
 
 class File {
-    public $file;
-    public $name;
-    public $type;
-    public $size;
+    private $file;
+    private $fileName;
+    public $fileType;
+    public $fileSize;
+    private $originalfilePath;
+    private $uploadPath = UPLOAD_FILES;
 
     public function __construct($file){
-        $this->file = $file;
-        $this->name = $_FILES['fileInput']['name'];
-        $this->type = $_FILES['fileInput']['type'];
-        $this->size = $_FILES['fileInput']['size'];
+        $this->fileName = $_FILES['fileInput']['name'];
+        $this->fileType = $_FILES['fileInput']['type'];
+        $this->fileSize = $_FILES['fileInput']['size'];
+        $this->originalfilePath = $_FILES['fileInput']['tmp_name'];
+        $this->fileError = $_FILES['fileInput']['error'];
 
+    }
+
+    public function getOriginalFilePath(){
+        return $this->originalfilePath;
     }
 
     public function getFileName() {
@@ -24,5 +31,41 @@ class File {
 
     public function getFileSize() {
         return $this->size;
+    }
+
+    public function getUploadPath(){
+        return $this->uploadPath;
+    }
+
+    public function setFileName($name) {
+        $this->fileName = $name;
+    }
+
+    public function addFile(){
+        try {
+            $target = $this->getUploadPath() . $this->getFileName;
+            // if(move_uploaded_file($this->getOriginalFilePath(), $target)){
+            if(move_uploaded_file($_FILES['fileInput']['tmp_name'], 'images/'.$_FILES['fileInput']['name'])){
+                include './connect.db.php';
+                $sql='INSERT INTO files
+                    (date, fileName, fileType, fileSize)
+                VALUES(
+                        NOW(),
+                        :fileName,
+                        :fileType,
+                        :fileSize
+                    )';
+                    $s = $pdo->prepare($sql);
+
+                    $s->bindValue(':fileName',$this->fileName);
+                    $s->bindValue(':fileType',$this->fileType);
+                    $s->bindValue(':fileSize',$this->fileSize);
+                    $s->execute();
+                    $alert = new Alert('success', 'Plik został dodany');
+                    $alert->show();
+            }
+        } catch (PDOException $e) {
+            echo '<p>'.$e->getLine().' --- '.$e->getMessage().'</p>';
+        }
     }
 }
--- sites/install.php
+++ sites/install.php
@@ -17,4 +17,38 @@
 
     $alert = new Alert('success', 'Utworzono tabelę images.');
     $alert->show();
+
+    try {
+        include './connect.db.php';
+        $sql='CREATE TABLE IF NOT EXISTS gallery (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            date DATETIME NOT NULL,
+            name VARCHAR(255)
+        ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+        $pdo->exec($sql);
+
+    } catch (PDOException $e) {
+        echo '<p>'.$e->getLine().' --- '.$e->getMessage().'</p>';
+    }
+
+    $alert = new Alert('success', 'Utworzono tabelę gallery.');
+    $alert->show();
+
+    try {
+        include './connect.db.php';
+        $sql='CREATE TABLE IF NOT EXISTS files (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            date DATETIME NOT NULL,
+            fileName VARCHAR(255),
+            fileType VARCHAR(255),
+            fileSize TEXT
+        ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+        $pdo->exec($sql);
+
+    } catch (PDOException $e) {
+        echo '<p>'.$e->getLine().' --- '.$e->getMessage().'</p>';
+    }
+
+    $alert = new Alert('success', 'Utworzono tabelę files.');
+    $alert->show();
 ?>
