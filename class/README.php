50e86c8c8a451a0f190dfa7f16d8a38c062ff4c0
Damian Molinski <tedeer@gmail.com>
2022-04-08 21:53:08 +0200

    Added readme.php about

---
+++ class/README.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$_FILE['foto']['name']; // Określa nazwę pliku
+$_FILE['foto']['type']; //Określa typ przesyłanego pliku, np: *.jpg, *.git, *.png
+$_FILE['foto']['size']; // Określa rozmiar pliku ---> w bajtach
+$_FILE['foto']['tmp_name']; // Określa tymczasową lokalizację pliku na serwerze
+$_FILE['foto']['error']; // Określa informacje o kodzie błędu - 0 (zero) to sukces, każdy inny to błąd.
