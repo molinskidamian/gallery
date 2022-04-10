0618fbfe7af60dad70eb97f023efb0c94a6fa4ab
Damian Molinski <tedeer@gmail.com>
2022-04-10 16:20:59 +0200

    You have to consider (ang.rzemyslec) this code

--- sites/login.php
+++ sites/login.php
@@ -5,7 +5,14 @@ if(!isset($_POST['sub-login'])){
 } else {
     echo "<p>FORMULARZ został wysłany</p>";
     echo "<p>Przyjmuje dane</p>";
-    $form = new Form($_POST['sub-login']);
-    echo '<p>'.$form->isEmpty($_POST['login']).'</p>';
-    echo '<p>'.$form->isEmpty($_POST['password']).'</p>';
+    // $form = new Form($_POST['sub-login']);
+    $user = new User($_POST['sub-login']);
+    $user->setSession($_POST['login'], $_POST['password']);
+
+    // echo '<p>'.$form->isEmpty($_POST['login']).'</p>';
+    // echo '<p>'.$form->isEmpty($_POST['password']).'</p>';
+    echo '<p>Session Login: '.$user->getSessionLogin().'</p>';
+    echo '<p>Session Password '.$user->getSessionPassword().'</p>';
+
+
 }
